2b6e165bc2363166300fc191be83c832c09fe967
marklabenski <mark.labenski@gmail.com>
2017-06-21 00:34:41 +0200

    feat: add fixture loading and connection setup

---
+++ api/src/db/loadFixtures.js
@@ -0,0 +1,27 @@
+const CityContract = require('./models/cityContracts.js');
+const fs = require('fs');
+const { promisify } = require('util');
+
+const readFs = promisify(fs.readFile);
+module.exports = new Promise((resolve) => {
+  if (!process.env.LOAD_FIXTURES ||
+    !process.env.LOAD_FIXTURES === 'true') {
+    resolve();
+  }
+  readFs(`${process.cwd()}/src/db/fixtures/data.json`).then((serJsonData) => {
+    const jsonData = JSON.parse(serJsonData);
+    jsonData.forEach((cityContractData) => {
+      const newC = new CityContract(cityContractData);
+      newC.save((err) => {
+        if (err) console.error(err);
+      });
+    });
+
+    resolve();
+    // CityContract.find((err, data) => {
+    //   return data.forEach((citycon, index) => {
+    //     console.log('loaded fixture nr ', index);
+    //   });
+    // });
+  });
+});
