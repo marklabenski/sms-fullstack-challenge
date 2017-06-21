79fe019bd003b8c91fb4645700259a0e8f6e1575
marklabenski <mark.labenski@gmail.com>
2017-06-21 22:48:31 +0200

    fix: add CORS header

--- api/src/server.js
+++ api/src/server.js
@@ -10,6 +10,15 @@ app.set('host', process.env.HOST || '0.0.0.0');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
