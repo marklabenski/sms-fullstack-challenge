d2b1c8bcc6e955bf9a1d0655a8cae442354cdaa9
marklabenski <mark.labenski@gmail.com>
2017-06-21 14:43:24 +0200

    feat: start server and use cityContacts controller

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dbSetup = require('./db/setup');
+const cityContractsController = require('./controllers/cityContracts.js');
 
 const app = express();
 
@@ -8,8 +9,16 @@ app.set('host', process.env.HOST || '0.0.0.0');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.use('/city_contracts', cityContractsController);
+
 dbSetup
 .then((connection) => {
   console.log(connection);
 })
 .catch(err => console.error(err));
+
+app.listen(
+  app.get('port'),
+  app.get('host'),
+  () => { /* server listening */ },
+);
