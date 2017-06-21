185b21508c06ea541b2b491fab794ace1076d45e
marklabenski <mark.labenski@gmail.com>
2017-06-21 16:02:55 +0200

    feat: add other crud functions

--- api/src/services/cityContractService.js
+++ api/src/services/cityContractService.js
@@ -2,6 +2,15 @@ const CityContracts = require('../db/models/cityContracts');
 
 const service = {};
 
+const getLastCityConId = () =>
+  new Promise((resolve, reject) => {
+    CityContracts.findOne().sort({ id: -1 }).exec((err, cityCon) => {
+      if (err) reject(`${err.name} : ${err.message}`);
+      if (cityCon) resolve(cityCon.id + 1);
+      else resolve(1);
+    });
+  });
+
 const list = () =>
   new Promise((resolve, reject) => {
     CityContracts.find({}, (err, cityCons) => {
@@ -11,6 +20,58 @@ const list = () =>
     });
   });
 
+const create = cityConParam =>
+  new Promise((resolve, reject) => {
+    getLastCityConId()
+    .then((nextId) => {
+      const newCityCon = new CityContracts({
+        id: nextId,
+        city: cityConParam.city,
+        start_date: cityConParam.start_date,
+        end_date: cityConParam.end_date,
+        price: cityConParam.price,
+        status: cityConParam.status,
+        color: cityConParam.color,
+      });
+      newCityCon.save((err) => {
+        if (err) reject(`${err.name} : ${err.message}`);
+        else resolve(`City Contract saved: ${newCityCon.id}`);
+      });
+    }).catch();
+  });
+
+const update = (cityConId, cityConParam) =>
+  new Promise((resolve, reject) => {
+    CityContracts.findOneAndUpdate(
+      { id: cityConId },
+      cityConParam,
+      (err) => {
+        if (err) reject(`${err.name} : ${err.message}`);
+        else resolve(`Updated CityCon with id ${cityConId}.`);
+      },
+    );
+  });
+
+// underscore prefix to prevent collision with reserved word
+const _delete = (cityConId) => {
+  console.log(`trying to delete ${cityConId}`);
+  return new Promise((resolve, reject) => {
+    CityContracts.findOneAndRemove(
+      { id: cityConId },
+      (err, doc) => {
+        if (err) reject(`${err.name} : ${err.message}`);
+        else {
+          console.log(doc);
+          resolve('CityCon has been deleted');
+        }
+      },
+    );
+  });
+};
+
 service.list = list;
+service.create = create;
+service.update = update;
+service._delete = _delete;
 
 module.exports = service;
