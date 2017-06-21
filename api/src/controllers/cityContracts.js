800b6b38847a4aa0d592b759dfcef25ea06d417a
marklabenski <mark.labenski@gmail.com>
2017-06-21 14:43:48 +0200

    feat: add cityContracts controller and service with list route

---
+++ api/src/controllers/cityContracts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cityContractService = require('../services/cityContractService');
+
+const router = express.Router();
+
+const list = (req, res) => {
+  cityContractService.list()
+  .then(cityContracts => res.status(200).json(cityContracts))
+  .catch(err => res.status(400).send(err));
+};
+
+router.get('/list', list);
+
+module.exports = router;
